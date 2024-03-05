0ccc16aef5c0875046c0d5f8fe1bee63bd61b683
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-05 16:37:32 +0000

    chore: install react router and prepare router components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
+
 import './App.css'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 function App() {
 
-  return (
-    <>
-      <p>Hello World</p>
-    </>
-  )
+    return (
+        <>
+            <p>Hello World</p>
+
+            <BrowserRouter>
+
+                <Routes>
+
+                </Routes>
+
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
