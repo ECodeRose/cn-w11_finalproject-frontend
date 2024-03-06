a77ed9fe22546a7d0d230f2fd1bd5f58467c322e
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-06 11:54:05 +0000

    feature: add non functioning query form

--- src/components/QueryForm.jsx
+++ src/components/QueryForm.jsx
@@ -1,8 +1,17 @@
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import "./QueryForm.css"
+
 export const QueryForm = (props) => {
     return (
-        <>
+        <div className="query">
             <p>Query Form</p>
-        </>
+            <form className="query-form">
+                <input className="locations-earch" placeholder="Location"></input>
+                <DatePicker placeholder="Select a date"/>
+                <button>Send Query</button>
+            </form>
+        </div>
     )
 }
 
