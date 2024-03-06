4c783d5270192934ed8880fe245abd59577dbd32
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-06 13:42:36 +0000

    feature: query form

--- src/components/QueryForm.jsx
+++ src/components/QueryForm.jsx
@@ -1,14 +1,21 @@
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import "./QueryForm.css"
+import { useState } from "react"
 
 export const QueryForm = (props) => {
+    const [date, setDate] = useState(new Date());
+
     return (
         <div className="query">
             <p>Query Form</p>
             <form className="query-form">
-                <input className="locations-earch" placeholder="Location"></input>
-                <DatePicker placeholder="Select a date"/>
+                <input className="location-search" placeholder="Location"></input>
+                <DatePicker 
+                    showicon 
+                    selected={date}
+                    dateFormat="dd MMM yyyy"
+                />
                 <button>Send Query</button>
             </form>
         </div>
