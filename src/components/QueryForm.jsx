573b1b950ee1d28f1bac9e7fcb1ac897b6390df1
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-06 14:42:23 +0000

    feature: form submits request to backend

--- src/components/QueryForm.jsx
+++ src/components/QueryForm.jsx
@@ -2,21 +2,39 @@ import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import "./QueryForm.css"
 import { useState } from "react"
+import { getRequest, postRequest } from "../common/requests"
 
 export const QueryForm = (props) => {
     const [date, setDate] = useState(new Date());
+    const [location, setLocation] = useState(null);
+
+    const sendRequest = () => {
+        console.log(date, "\n", location);
+
+        const reqBody = JSON.stringify({
+            date: date,
+            location: location,
+        });
+
+        postRequest(`${import.meta.env.VITE_SERVER_URL}/getweather`, reqBody);
+    }
 
     return (
-        <div className="query">
-            <p>Query Form</p>
-            <form className="query-form">
-                <input className="location-search" placeholder="Location"></input>
+        <div className="request">
+            <p>Request Form</p>
+            <form className="request-form" onSubmit={(e) => e.preventDefault()}>
+                <input 
+                    className="location-search" 
+                    placeholder="Location"
+                    onChange={(e) => setLocation(e.target.value)}
+                ></input>
                 <DatePicker 
                     showicon 
                     selected={date}
                     dateFormat="dd MMM yyyy"
+                    onChange={(calendarDate) => setDate(calendarDate)}
                 />
-                <button>Send Query</button>
+                <button onClick={sendRequest}>Send Request</button>
             </form>
         </div>
     )
