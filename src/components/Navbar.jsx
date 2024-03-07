5843ad154af5eb39824a8925df34b08747ceead3
reubendass <reubendass98@gmail.com>
2024-03-07 15:43:22 +0000

    navbar mock

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import UserInfo from "./UserInfo"
+import React, { useState } from "react";
+import UserInfo from "./UserInfo";
+import "./Navbar.css";
 
 export const Navbar = (props) => {
-    return (
-        <>
-            <p>Navbar with links</p>
-            <UserInfo />
-        </>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  return (
+    <nav className="navbar">
+      <button onClick={() => setIsOpen(!isOpen)} className="navbar-toggle">
+        Toggle Navbar
+      </button>
+      {isOpen && (
+        <div className="navbar-menu">
+          <p className="navbar-link">Navbar with links</p>
+          <UserInfo />
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/page-user.jsx
+++ src/pages/page-user.jsx
@@ -1,9 +1,12 @@
+import LoginOrRegister from "../components/LoginOrRegister";
+import QueryForm from "../components/QueryForm";
+import QueryResponse from "../components/QueryResponse";
 const PageUser = (props) => {
-    return (
-        <>
-            <p>User Page</p>
-        </>
-    )
-}
+  return (
+    <>
+      <p>User Page</p>
+    </>
+  );
+};
 
 export default PageUser;
