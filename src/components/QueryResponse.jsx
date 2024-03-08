1e00bacc4f0b3d98175dd84e7dc690413ef09fb8
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-08 11:42:30 +0000

    feature: display map in response

--- src/components/QueryResponse.jsx
+++ src/components/QueryResponse.jsx
@@ -18,6 +18,8 @@ export const QueryResponse = (props) => {
                     <p>Showing weather information for {location.city}, {location.county} in {location.country}</p>
                     <p>Today{"'"}s weather is {weather.weathername[0]}.</p>
                     <p>The highest temperature today will be {weather.temperature[0]} degrees celsius.</p>
+
+                    <img src={`data:image/png;base64,${props.response.map}`} />
                     <p className="response-wrong-prompt">Wrong place? Try using a postcode or search with a more specific location name (eg, Bury, Greater Manchester)</p>
                 </>
             )
