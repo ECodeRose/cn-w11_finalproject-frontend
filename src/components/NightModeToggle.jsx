a1c28daa87d985624e562ea1bd9806f0f8f86db4
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-03-09 13:46:15 +0000

    feature: dark mode toggle functionality

--- src/components/NightModeToggle.jsx
+++ src/components/NightModeToggle.jsx
@@ -1,8 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoMdSunny, IoMdMoon } from "react-icons/io";
 
 export const NightModeToggle = () => {
     const [nightMode, setMode] = useState(false);
+
+    useEffect(() => {
+
+        const variables = Array.from(document.styleSheets)
+        .filter(styleSheet => {
+            return styleSheet.cssRules;
+        })
+        .map(styleSheet => Array.from(styleSheet.cssRules))
+        .flat()
+        .filter(cssRule => cssRule.selectorText === ':root')
+        .map(cssRule => cssRule.cssText.split('{')[1].split('}')[0].trim().split(';'))
+        .flat()
+        .filter(text => text !== "")
+        .map(text => text.split(':'))
+        .map(parts => ({key: parts[0].trim(), value: parts[1].trim() }))
+
+        variables.forEach((variable) => {
+            if (variable.key.indexOf("Toggleable") != -1) {
+                const newValue = variable.key.replace("Toggleable", nightMode ? "Night" : "Day")
+                console.log(variable.key, `var(${newValue})`);
+                document.documentElement.style.setProperty(variable.key, `var(${newValue})`);
+            }
+        });
+
+    }, [nightMode]);
 
     return (
         <div className="nightmode-toggle" onClick={()=>setMode(!nightMode)}>
